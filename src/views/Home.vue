f8ba9e9653c11129a96394890dbd3de512968b16
fanzheng <montage_fz@sina.com>
2019-10-14 17:26:45 +0800

    - 使用 vue.observable 这个属性来实现让一个对象的响应. 从而达到最小化跨组件状态存储

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,12 @@
 <template>
     <div class="home">
         <div class="change-view">
-            <router-link to="/freeze">使用Object.freeze优化vue组件渲染性能</router-link>
+            <p>
+                <router-link to="/freeze">使用Object.freeze优化vue组件渲染性能</router-link>
+            </p>
+            <p>
+                <router-link to="/observable">使用vue.observable实现最小跨组件状态存储器</router-link>
+            </p>
         </div>
     </div>
 </template>
@@ -12,8 +17,7 @@
         name: 'home',
         components: {},
         data() {
-            return {
-            };
+            return {};
         },
         created() {
         },
